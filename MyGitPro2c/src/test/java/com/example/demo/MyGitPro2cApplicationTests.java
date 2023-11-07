95391a98a2dcffc52eb76db02e96bda4b1e7b0f2
kagura08111 <kagura13579@gmail.com>
2023-11-07 12:11:54 +0900

    test

---
+++ MyGitPro2c/src/test/java/com/example/demo/MyGitPro2cApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGitPro2cApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
